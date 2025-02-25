5d9a4df72b18bd89c9d2d30f8a846f40c065e58f
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-02-26 00:38:45 +0530

    Added env files to quiz-service

--- src/main/java/com/zephyra/quiz_service/QuizServiceApplication.java
+++ src/main/java/com/zephyra/quiz_service/QuizServiceApplication.java
@@ -2,12 +2,17 @@ package com.zephyra.quiz_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizServiceApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 	}
 
 }
