4569674e425d217b04230b131fd20680d0753d76
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-02-26 00:24:56 +0530

    Added the env files

--- src/main/java/com/zephyra/question_service/QuestionServiceApplication.java
+++ src/main/java/com/zephyra/question_service/QuestionServiceApplication.java
@@ -2,12 +2,15 @@ package com.zephyra.question_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class QuestionServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionServiceApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 	}
-
 }
