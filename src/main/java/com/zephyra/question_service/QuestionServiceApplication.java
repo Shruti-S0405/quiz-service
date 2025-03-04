dd9a8940f6197938b6d2c71f7a42fba3e3b07798
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-03-05 00:06:48 +0530

    Final commit issue not fixed

--- src/main/java/com/zephyra/question_service/QuestionServiceApplication.java
+++ src/main/java/com/zephyra/question_service/QuestionServiceApplication.java
@@ -1,5 +1,4 @@
 package com.zephyra.question_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -7,10 +6,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class QuestionServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(QuestionServiceApplication.class, args);
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+
+		SpringApplication.run(QuestionServiceApplication.class, args);
 	}
 }
